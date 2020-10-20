c946daaa9b1b3aec930506a31e0fb31d97669176
rosaga <richardjohnzranger@gmail.com>
2020-10-20 09:07:34 +0300

    new changes

--- dist/js/templates/components/header.js
+++ dist/js/templates/components/header.js
@@ -6,7 +6,7 @@ const header_template = `
         </a>
         <a class="navbar-brand" href="index.html">
             <span class="logo-text">
-                <img src="../assets/images/index.png" style="max-height: 56px" alt="homepage" class="light-logo" />
+                <img src="../assets/images/nascop-logo.png" style="max-height: 56px" alt="homepage" class="light-logo" />
             </span>
         <a class="topbartoggler d-block d-md-none waves-effect  waves-light" href="javascript:void(0)" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <i class="ti-more"></i>
